8d118a999df20df910e8554efd26b426de78b7e9
kurniawans putra <kurniawansputra02@gmail.com>
2021-05-24 09:32:24 +0700

    first commit

---
+++ myscript.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    nav:true,
+    margin:10,
+    dots:false,
+    padding:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        400:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
